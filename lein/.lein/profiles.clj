84ef77eac2d7852d71f19917106a6b31a75b976c
Alex Podaras <apodaras@gmail.com>
2016-12-13 10:48:20 +0200

    Simplify repl profile

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,20 +1,3 @@
-{:user {:plugins [[lein-ancient "0.6.8"]
+{:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-try "0.4.3"]]}
- :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [im.chit/vinyasa "0.4.2"]
-                       [alembic "0.3.2"]]
-        :injections
-        [(require '[vinyasa.inject :as inject])
-         (inject/in
-          [vinyasa.inject :refer [inject [in inject-in]]]
-          [clojure.pprint pprint]
-          [clojure.java.shell :refer [sh]]
-
-          ;; Dependency distiller
-          [alembic.still :refer [[load-project load-project-clj] [distill pull]]]
-
-          clojure.core
-          [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var])]}}
-
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]] }}
