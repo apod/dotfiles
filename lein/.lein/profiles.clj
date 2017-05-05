2f2b577b62e1e72abba7293613e2f18e66b3c43e
Alex Podaras <alex@kinems.com>
2017-05-05 11:39:40 +0300

    Add lucid debugging fns, and pull

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-try "0.4.3"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [im.chit/lucid.core.debug "1.3.6"]
+                       [im.chit/lucid.package "1.3.6"]
                        [spyscope "0.1.6"]]
-        :injections [(require 'spyscope.core)]}}
+        :injections [(require '[spyscope.core :refer :all])
+                     (require '[lucid.core.debug :refer :all])
+                     (require '[lucid.package :refer [pull]])]}}
