12407b464a0117cf3068f23454fef17a7f827f0b
Alex Podaras <alex@kinems.com>
2017-05-05 10:04:35 +0300

    Add spyscope

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-try "0.4.3"]]}
- :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]] }}
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [spyscope "0.1.6"]]
+        :injections [(require 'spyscope.core)]}}
