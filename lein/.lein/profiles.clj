d122b2f416e3b857d88c4dcae39541b7f081e3ff
Alex Podaras <alex@kinems.com>
2017-10-28 20:31:31 +0300

    Add plugins for formatting, linting etc.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
-                  [lein-try "0.4.3"]]}
+                  [lein-try "0.4.3"]
+                  [lein-cljfmt "0.5.6"]
+                  [venantius/yagni "0.1.4"]
+                  [lein-kibit "0.1.6-beta2"]
+                  [jonase/eastwood "0.2.4"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [im.chit/lucid.core.debug "1.3.6"]
                        [im.chit/lucid.package "1.3.6"]
